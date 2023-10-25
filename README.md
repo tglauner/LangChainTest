f692c0f563e1513cac95e7ca55fbaa232704a723
Tim Glauner <glaunertim@gmail.com>
2023-10-25 07:42:29 -0400

    Final version that runs. No longer maintained.

--- README.md
+++ README.md
@@ -1,2 +1,10 @@
 # LangChainTest
 Just testing LangChain and play around with it
+
+Following broadly https://towardsdatascience.com/4-ways-of-question-answering-in-langchain-188c6707cc5a
+
+Program:
+1. Read Lehman pdf
+2. Ask to summarize
+3. Ask 10 main points
+4. Ask beamer latex presentation 
--- app.py
+++ app.py
@@ -2,13 +2,26 @@
 import os
 from dotenv import load_dotenv
 from langchain.llms import OpenAI
+from langchain.chains.question_answering import load_qa_chain
+from langchain.document_loaders import PyPDFLoader
 
 print("Start")
 load_dotenv()
 
-llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY"), temperature=0.9) 
-text = "How are you?"
+llm = OpenAI(openai_api_key=os.getenv("OPENAI_API_KEY")) 
+#text = "How are you?"
+#print(llm(text))
 
-print(llm(text))
+# load document
+loader = PyPDFLoader("LEH The Swap Curve.pdf")
+documents = loader.load() 
 
+# ask document some questions
+chain = load_qa_chain(llm=llm, chain_type="map_reduce")
+query = "Summarize the document in 100 words"
+print("Query:"+query)
+print(chain.run(input_documents=documents, question=query),"\n")
+query = "What are the main ten points of the document?"
+print("Query:"+query)
+print(chain.run(input_documents=documents, question=query),"\n")
 print("End")
